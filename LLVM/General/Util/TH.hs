84e77d5cf8da1484052d9e8bed034e70e8279bd1
Daniel Fox Franke <dfoxfranke@akamai.com>
2014-06-04 18:31:26 -0400

    Initial commit.

---
+++ LLVM/General/Util/TH.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, ParallelListComp #-}
+
+module LLVM.General.Util.TH(genStrEnum) where
+
+import Data.Char
+import Data.Data(Data)
+import Data.Functor
+import Data.Maybe
+import Data.Typeable(Typeable)
+import Language.Haskell.TH
+
+mkDconName :: String -> String -> Name
+mkDconName prefix [] = mkName prefix
+mkDconName prefix (x:xs) = 
+  mkName $ prefix ++ normalizeHead x : (normalizeTail <$> xs)
+  where normalizeHead x
+          | isAlpha x || isDigit x = toUpper x
+          | otherwise = '_'
+        normalizeTail x
+          | isAlpha x || isDigit x = toLower x
+          | otherwise = '_'
+
+genStrEnum :: String -> String -> [String] -> String -> Bool -> Q [Dec]
+genStrEnum tycon dconPrefix strs defaultStr wantParse =
+  do lowerStr <- newName "lowerStr"
+     inputStr <- newName "inputStr"
+     let tyconName = mkName tycon
+     let parseName = mkName $ "parse" ++ tycon
+     let showName  = mkName $ "show" ++ tycon
+     let dconNames = mkDconName dconPrefix <$> strs
+     dataDecl <- 
+       dataD (cxt []) tyconName []
+       (flip normalC [] <$> dconNames)
+       [''Eq, ''Ord, ''Show, ''Enum, ''Bounded, ''Typeable, ''Data]
+     parseDecl <- sigD parseName [t|String -> Maybe $(conT tyconName)|]
+     parseDef <-
+       funD parseName
+       [clause 
+        [varP inputStr]
+        (guardedB $
+         [normalGE [|$(varE lowerStr) == $(litE $ stringL (map toLower str))|]
+                   [|Just $(conE dconName)|]
+          | str <- strs | dconName <- dconNames] ++
+         [normalGE [|otherwise|] [|Nothing|]])
+         [head <$> [d|$(varP lowerStr) = map toLower $(varE inputStr)|]]]
+     showDecl <- sigD showName [t|Maybe $(conT tyconName) -> String|]
+     showDef <-
+       funD showName $
+       [clause [conP 'Just [conP dconName []]] (normalB (litE (stringL str))) []
+        | str <- strs | dconName <- dconNames] ++
+       [clause [conP 'Nothing []] (normalB (litE (stringL defaultStr))) []]
+     return $ if wantParse then [dataDecl,parseDecl,parseDef,showDecl,showDef]
+              else [dataDecl,showDecl,showDef]
